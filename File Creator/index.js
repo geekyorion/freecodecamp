80346dc76ffabfe91ed4eeb03cdaf6a3ec902a84
Shashank Sharma <shashank.ns67@gmail.com>
2020-01-15 23:56:07 +0530

    Automatic file creation script added + FreeCodeCamp n/w call json data + node env based script

---
+++ File Creator/index.js
@@ -0,0 +1,35 @@
+// import json data and file system module
+const json = require('./data');
+const fs = require('fs');
+
+// fetch all of the edges
+var probs = json.result.data.allChallengeNode.edges;
+
+/**
+ * Note: use node to create file(s)
+ * run "node index.js"
+ * it will create the files in the same folder
+ */
+
+var count = 1;
+for (let i = 0; i < probs.length; i++) {
+    if (probs[i].node.block == "basic-javascript") {
+
+        // Fetch the title
+        var title = probs[i].node.title;
+        // split title into words array and join using "-"
+        title = title.split(" ").join("-");
+        // calculate the number of zeros as per the value of count
+        title = `${count < 10 ? "00" + count : count < 100 ? "0" + count : count}-${title}.js`;
+
+        // create link to store in the file
+        var data = `\n\n// link: https://www.freecodecamp.org${probs[i].node.fields.slug}`;
+
+        // use writeFile to write and save file in the same folder (uses relative path)
+        fs.writeFile(title, data, (err) => {
+            if (err) throw err;
+        });
+
+        count++;
+    }
+}
