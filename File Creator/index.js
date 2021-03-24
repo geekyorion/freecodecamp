9492e6ce3699bd20023e6d0aa1206caae5d99e88
Shashank Sharma <shashank.ns67@gmail.com>
2021-03-24 22:45:52 +0530

    markup is generated using node js index file

--- File Creator/index.js
+++ File Creator/index.js
@@ -3,7 +3,7 @@ const json = require('./data');
 const fs = require('fs');
 
 // fetch all of the edges
-var probs = json.allChallengeNode.edges;
+const probs = json.allChallengeNode.edges;
 
 /**
  * Note: use node to create file(s)
@@ -11,21 +11,38 @@ var probs = json.allChallengeNode.edges;
  * it will create the files in the same folder
  */
 
-var count = 0;
+let count = 0;
 for (let i = 0; i < probs.length; i++) {
     if (probs[i].node.block == "bootstrap") {
 
         // Fetch the title
-        var title = probs[i].node.title;
+        let title = probs[i].node.title;
         // split title into words array and join using "-"
         title = title.split(" ").join("-");
         // calculate the number of zeros as per the value of count
         title = `${count < 10 ? "00" + count : count < 100 ? "0" + count : count}-${title}.html`;
 
-        // create link to store in the file
-        var data = `\n\n<!-- link: https://www.freecodecamp.org${probs[i].node.fields.slug} -->`;
-        data += `\n<!-- For the live view, please visit the below link -->`;
-        data += `\n<!-- https://geekyorion.github.io/freecodecamp/Front End Development Libraries/Bootstrap/${title}\n -->`;
+        // don't add the space as it can cause extra space
+        const data = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FCC - Front End Development Libraries - Boostrap</title>
+    <link rel="stylesheet" href="./../assets/bootstrap-v5/css/bootstrap.min.css">
+</head>
+<body>
+
+    <script src="./../assets/bootstrap-v5/js/bootstrap.min.js"></script>
+</body>
+</html>
+
+<!-- link: https://www.freecodecamp.org${probs[i].node.fields.slug}
+For the live view, please visit the below link
+https://geekyorion.github.io/freecodecamp/Front End Development Libraries/Bootstrap/${title} -->
+`;
 
         // use writeFile to write and save file in the same folder (uses relative path)
         fs.writeFile(title, data, (err) => {
--- README.md
+++ README.md
@@ -8,7 +8,7 @@
     -   Links for the problems are added at the end of the file
     -   Projects are hosted on github. Please visit the project section on the same page
 -   #### Front End Development Libraries
-    -   FCC is using bootstrap v3.7 but in the solution we are using latest bootstrap (v5)
+    -   FCC is using bootstrap v3.7 but in the solution I'm using latest bootstrap (v5)
     -   Latest bootstrap is downloaded and saved in the course folder.
     -   If the solution differs due to different version of bootstrap, then bootstrap v3 solution (that needs to be submitted) is commented.
     -   Links for the problems are added at the end of the file with live view link (iff it's an HTML file)
