cd76e1c7cf4329a06d0d6cffb8a896607429e95f
Shashank Sharma <shashank.ns67@gmail.com>
2021-04-03 23:03:32 +0530

    added solutions for react section

---
+++ Front End Development Libraries/React/046-Render-React-on-the-Server-with-renderToString.jsx
@@ -0,0 +1,15 @@
+import { ReactDOMServer } from '../assets/react/react-dom.production.min/server';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return <div>asdfasdf</div>
+    }
+};
+  
+// Change code below this line
+ReactDOMServer.renderToString(<App />);
+
+// This is useful in case of SEO when you want some markup once initial loading is done
