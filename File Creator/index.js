75283e91c0722006267b41c712820ff9fdaede73
Shashank Sharma <shashank.ns67@gmail.com>
2021-11-07 23:58:29 +0530

    file creator format according to AJAX section under Data Visualization Course with generated files

--- File Creator/index.js
+++ File Creator/index.js
@@ -40,10 +40,48 @@ for (let i = 0; i < probs.length; i++) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FCC - ${courseName} - ${key}</title>
+    <style>
+        body {
+            text-align: center;
+            font-family: "Helvetica", sans-serif;
+        }
+        h1 {
+            font-size: 2em;
+            font-weight: bold;
+        }
+        .box {
+            border-radius: 5px;
+            background-color: #eee;
+            padding: 20px 5px;
+        }
+        button {
+            color: white;
+            background-color: #4791d0;
+            border-radius: 5px;
+            border: 1px solid #4791d0;
+            padding: 5px 10px 8px 10px;
+        }
+        button:hover {
+            background-color: #0F5897;
+            border: 1px solid #0F5897;
+        }
+    </style>
 </head>
 <body>
-    <div id="root"></div>
-    <script src="./../assets/d3.v7.min.js"></script>
+    <h1>Cat Photo Finder</h1>
+    <p class="message box">
+        The message will go here
+    </p>
+    <p>
+    <button id="getMessage">
+        Get Message
+    </button>
+    </p>
+    <script>
+        document.addEventListener('DOMContentLoaded', () => {
+
+        });
+    </script>
 </body>
 </html>
 <!-- link: ${probs[i].link}
